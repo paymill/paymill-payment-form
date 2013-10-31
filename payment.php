69ec6670d682805f760bd88f3c0f2a78da4b0b38
Thilo Hoffstadt <thilo.hoffstadt@payintelligent.de>
2013-10-31 15:25:23 +0100

    PM-756 Changed the payment routine to use the latest lib version

--- payment.php
+++ payment.php
@@ -10,54 +10,48 @@
         // and put the containing "lib" folder into your project
         //
 
-        define('PAYMILL_API_HOST', 'https://api.paymill.com/v2/');
-        define('PAYMILL_API_KEY', 'YOUR_API_KEY');
-        set_include_path(
-                implode(PATH_SEPARATOR, array(
-                    realpath(realpath(dirname(__FILE__)) . '/lib'),
-                    get_include_path())
-                )
-        );
+       define('PAYMILL_API_KEY', 'YOUR_API_KEY');
+       require 'vendor/autoload.php';
 
         if (isset($_POST['paymillToken'])) {
-            require "Services/Paymill/Transactions.php";
-            require "Services/Paymill/Clients.php";
-            require "Services/Paymill/Payments.php";
+            $service = new Paymill\Request(PAYMILL_API_KEY);
+            $client = new Paymill\Models\Request\Client();
+            $payment = new Paymill\Models\Request\Payment();
+            $transaction = new \Paymill\Models\Request\Transaction();
 
-            $transactionsObject = new Services_Paymill_Transactions(PAYMILL_API_KEY, PAYMILL_API_HOST);
-            $clientsObject = new Services_Paymill_Clients(PAYMILL_API_KEY, PAYMILL_API_HOST);
-            $paymentsObject = new Services_Paymill_Payments(PAYMILL_API_KEY, PAYMILL_API_HOST);
+            try{
+                $client->setEmail('testuser@payintelligent.de');//'Some Testemail');
+                $client->setDescription('This is a Testuser.');
+                $clientResponse = $service->create($client);
 
-            $clientsParam = array(
-                'email' => 'Some Testemail',
-                'description' => 'This is a Testuser.'
-            );
-            $client = $clientsObject->create($clientsParam);
+                $payment->setToken($_POST['paymillToken']);
+                $payment->setClient($clientResponse->getId());
+                $paymentResponse = $service->create($payment);
 
-            $paymentsParam = array(
-                'token' => $_POST['paymillToken'],
-                'client' => $client['id']
-            );
-            $payment = $paymentsObject->create($paymentsParam);
+                $transaction->setPayment($paymentResponse->getId());
+                $transaction->setAmount($_POST['amount'] * 100);
+                $transaction->setCurrency($_POST['currency']);
+                $transaction->setDescription('Test Transaction');
+                $transactionResponse = $service->create($transaction);
 
-            $transactionsParam = array(
-                'payment' => $payment['id'],
-                'amount' => $_POST['amount'] * 100,
-                'currency' => $_POST['currency'],
-                'description' => 'Test Transaction'
-            );
-            $transaction = $transactionsObject->create($transactionsParam);
+                $title = "<h1>We appreciate your purchase!</h1>";
+                $result = print_r($transactionResponse, true);
+            } catch (\Paymill\Services\PaymillException $e){
+                $title = "<h1>An error has occoured!</h1>";
+                $result = print_r($e->getResponseCode(), true) ." <br />" . print_r($e->getResponseCode(), true) ." <br />" .print_r($e->getErrorMessage(), true);
+            }
+
         }
-        ?>
+       ?>
     </head>
     <body>
-        <div>
-            <h1>We appreciate your purchase!</h1>
+    <div>
+        <?php echo $title ?>
 
-            <h4>Transaction:</h4>
+        <h4>Transaction:</h4>
             <pre>
-                <?php print_r($transaction); ?>
+                <?php echo $result ?>
             </pre>
-        </div>
+    </div>
     </body>
 </html>

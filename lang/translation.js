3fe69cab62a1a0d6a16e809213ad286ab3a33ff1
Stefan Harenkamp <stefan.harenkamp@payintelligent.de>
2014-03-28 15:48:17 +0100

    PIB-59 : added combined form for elv/sepa

--- lang/translation.js
+++ lang/translation.js
@@ -17,6 +17,7 @@ translation["de"]["form"]["submit-button"] = 'Abschicken';
 translation["de"]["form"]["tooltip"] = "Hinter dem CVV-Code bzw. CVC verbirgt sich ein Sicherheitsmerkmal von Kreditkarten, &uuml;blicherweise handelt es sich dabei um eine drei- bis vierstelligen Nummer. Der CVV-Code befindet sich auf VISA-Kreditkarten. Der gleiche Code ist auch auf MasterCard-Kreditkarten zu finden, hier allerdings unter dem Namen CVC. Die Abk&uuml;rzung CVC steht dabei f&uuml;r Card Validation Code. Bei VISA wird der Code als Card Verification Value-Code bezeichnet. &Auml;hnlich wie bei Mastercard und VISA gibt es auch bei Diners Club, Discover und JCB eine dreistellige  Nummer, die meist auf der R&uuml;ckseite der Karte zu finden ist. Bei Maestro-Karten gibt es mit und ohne dreistelligen CVV. Wird eine Maestro-Karte ohne CVV verwendet kann einfach 000 eingetragen werden. American Express verwendet die CID (Card Identification Number). Dabei handelt es sich um eine vierstellige Nummer, die meist auf der Vorderseite der Karte, rechts oberhalb der Kartennummer zu finden ist.";
 //Elv
 translation["de"]["form"]["elv-paymentname"] = 'ELV';
+translation["de"]["form"]["elv-paymentname-advanced"] = 'SEPA';
 translation["de"]["form"]["elv-account"] = 'Kontonummer';
 translation["de"]["form"]["elv-holdername"] = 'Kontoinhaber';
 translation["de"]["form"]["elv-bankcode"] = 'Bankleitzahl';
@@ -52,6 +53,7 @@ translation["en"]["form"]["submit-button"] = 'Submit';
 translation["en"]["form"]["tooltip"] = "What is a CVV/CVC number? Prospective credit cards will have a 3 to 4-digit number, usually on the back of the card. It ascertains that the payment is carried out by the credit card holder and the card account is legitimate. On Visa the CVV (Card Verification Value) appears after and to the right of your card number. Same goes for Mastercard's CVC (Card Verfication Code), which also appears after and to the right of  your card number, and has 3-digits. Diners Club, Discover, and JCB credit and debit cards have a three-digit card security code which also appears after and to the right of your card number. The American Express CID (Card Identification Number) is a 4-digit number printed on the front of your card. It appears above and to the right of your card number. On Maestro the CVV appears after and to the right of your number. If you don’t have a CVV for your Maestro card you can use 000.";
 //Elv
 translation["en"]["form"]["elv-paymentname"] = 'Direct debit';
+translation["de"]["form"]["elv-paymentname-advanced"] = 'SEPA';
 translation["en"]["form"]["elv-account"] = 'Account number';
 translation["en"]["form"]["elv-holdername"] = 'Account holder';
 translation["en"]["form"]["elv-bankcode"] = 'Bank code';
